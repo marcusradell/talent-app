ebf4160143282a2d9d9b825dd69223ff70655d6e
Marcus Rådell <marcus@radell.net>
2025-11-18 11:14:36 +0100

    remove Developer component now that it became a single line

--- src/app/developers/[slug]/page.tsx
+++ src/app/developers/[slug]/page.tsx
@@ -4,12 +4,8 @@ type Props = {
   params: Promise<{ slug: string }>;
 };
 
-function Developer({ slug }: { slug: string }) {
-  return <CvWrapper slug={slug} />;
-}
-
 export default async function Page({ params }: Props) {
   const { slug } = await params;
 
-  return <Developer slug={slug} />;
+  return <CvWrapper slug={slug} />;
 }
