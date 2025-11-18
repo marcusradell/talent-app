3f186f8cc15fa4bd0053619dad0477689d40b6af
Marcus Rådell <marcus@radell.net>
2025-11-18 11:13:11 +0100

    simplify Developer component by removing suspense and fade in effect

--- src/app/developers/[slug]/page.tsx
+++ src/app/developers/[slug]/page.tsx
@@ -1,20 +1,11 @@
 import CvWrapper from "@/features/developer-profiles/components/cv/cv-wrapper";
-import { Suspense } from "react";
-import { BlurFade } from "@/components/magicui/blur-fade";
-import CvLoading from "./loading";
 
 type Props = {
   params: Promise<{ slug: string }>;
 };
 
 function Developer({ slug }: { slug: string }) {
-  return (
-    <Suspense fallback={<CvLoading />}>
-      <BlurFade delay={0.25} inView>
-        <CvWrapper slug={slug} />
-      </BlurFade>
-    </Suspense>
-  );
+  return <CvWrapper slug={slug} />;
 }
 
 export default async function Page({ params }: Props) {
